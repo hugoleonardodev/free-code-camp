347d342b43b15f00457a8b5cb04ce7f6d62ba730
hugoleonardodev <hugoleonardo.dev@gmail.com>
2020-12-18 20:39:59 -0300

    Record Collection

---
+++ javascript-algorithms-and-data-structures/record-collection.js
@@ -0,0 +1,37 @@
+// Setup
+var collection = {
+  2548: {
+    albumTitle: 'Slippery When Wet',
+    artist: 'Bon Jovi',
+    tracks: ['Let It Rock', 'You Give Love a Bad Name']
+  },
+  2468: {
+    albumTitle: '1999',
+    artist: 'Prince',
+    tracks: ['1999', 'Little Red Corvette']
+  },
+  1245: {
+    artist: 'Robert Palmer',
+    tracks: []
+  },
+  5439: {
+    albumTitle: 'ABBA Gold'
+  }
+};
+
+// Only change code below this line
+function updateRecords(object, id, prop, value) {
+  if (prop !== 'tracks' && value !== '') {
+    const array = Object.keys(object[id]);
+    if (array.includes(prop)) {
+      object[id][prop] = value;
+    } else {
+      const obj = {}
+      obj[prop] = value;
+      Object.assign(object[id], obj);
+    }
+  }
+  return object;
+}
+
+console.log(updateRecords(collection, 5439, 'artist', 'ABBA'));
