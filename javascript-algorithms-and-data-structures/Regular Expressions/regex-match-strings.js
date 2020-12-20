43eaf4ded6609c0505f5cb8879197e834445d2f7
hugoleonardodev <hugoleonardo.dev@gmail.com>
2020-12-19 21:13:05 -0300

    Regular Expressions

---
+++ javascript-algorithms-and-data-structures/Regular Expressions/regex-match-strings.js
@@ -0,0 +1,104 @@
+// regex and .testing()
+
+let petString = "James has a pet cat.";
+let petRegex = /dog|cat|bird|fish/; // Change this line
+let result = petRegex.test(petString);
+
+// ignore flag for all cases: FreeCodeCamp, freeCodeCamp, etc...
+
+let myString = "freeCodeCamp";
+let fccRegex = /freecodecamp/i; // Change this line
+let result = fccRegex.test(myString);
+
+//  extracting a matche with .match()
+
+let extractStr = "Extract the word 'coding' from this string.";
+let codingRegex = /coding/; // Change this line
+let result = extractStr.match(codingRegex); // Change this line
+
+// all matches with g flag
+
+let twinkleStar = "Twinkle, twinkle, little star";
+let starRegex = /Twinkle/ig; // Change this line
+let result = twinkleStar.match(starRegex); // Change this line
+
+// wild card or period matches a period of a string
+
+let exampleStr = "Let's have fun with regular expressions!";
+let unRegex = /.un/; // Change this line
+let result = unRegex.test(exampleStr);
+
+// multiple possibilities matches
+
+let bigStr = "big";
+let bagStr = "bag";
+let bugStr = "bug";
+let bogStr = "bog";
+let bgRegex = /b[aiu]g/;
+bigStr.match(bgRegex); // Returns ["big"]
+bagStr.match(bgRegex); // Returns ["bag"]
+bugStr.match(bgRegex); // Returns ["bug"]
+bogStr.match(bgRegex); // Returns null
+
+let quoteSample = "Beware of bugs in the above code; I have only proved it correct, not tried it.";
+let vowelRegex = /[aeiou]/ig; // Change this line
+let result = quoteSample.match(vowelRegex); // Change this line
+
+// matches in a range 
+
+let catStr = "cat";
+let batStr = "bat";
+let matStr = "mat";
+let bgRegex = /[a-e]at/;
+catStr.match(bgRegex); // Returns ["cat"]
+batStr.match(bgRegex); // Returns ["bat"]
+matStr.match(bgRegex); // Returns null
+
+let quoteSample = "The quick brown fox jumps over the lazy dog.";
+let alphabetRegex = /[a-z]/ig; // Change this line
+let result = quoteSample.match(alphabetRegex); // Change this line
+
+// match numbers and letters
+
+let jennyStr = "Jenny8675309";
+let myRegex = /[a-z0-9]/ig;
+// matches all letters and numbers in jennyStr
+jennyStr.match(myRegex);
+
+// matches all except the charset with ^
+
+let quoteSample = "3 blind mice.";
+let myRegex = /[^3aeiou]/gi; // Change this line
+let result = quoteSample.match(myRegex); // Change this line
+
+// matches one or more char in a row and return it with +
+
+let difficultSpelling = "Mississippi";
+let myRegex = /s+/g; // Change this line
+let result = difficultSpelling.match(myRegex); // ['ss', 'ss']
+
+// matches zero or more times and return with *
+
+let soccerWord = "gooooooooal!";
+let gPhrase = "gut feeling";
+let oPhrase = "over the moon";
+let goRegex = /go*/;
+soccerWord.match(goRegex); // Returns ["goooooooo"]
+gPhrase.match(goRegex); // Returns ["g"]
+oPhrase.match(goRegex); // Returns null
+
+// Only change code below this line
+let chewieRegex = /Aa*/; // Change this line
+// Only change code above this line
+
+let result = chewieQuote.match(chewieRegex);
+
+// lazy matching and greedy matching with ?
+
+let text = "<h1>Winter is coming</h1>";
+let myRegex = /<[a-z].?>/; // Change this line
+let result = text.match(myRegex);
+
+console.log(result);
+
+
