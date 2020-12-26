bac5c2159abebfdc67252e806b481c71eb56a73a
hugoleonardodev <hugoleonardo.dev@gmail.com>
2020-12-26 04:23:17 -0300

    Decimal to Roman

--- javascript-algorithms-and-data-structures/projects/convertToRoman.js
+++ javascript-algorithms-and-data-structures/projects/convertToRoman.js
@@ -1,20 +1,18 @@
-var roman = new Array();
-roman = ["M","CM","D","CD","C","XC","L","XL","X","IX","V","IV","I"];
-var decimal = new Array();
-decimal = [1000,900,500,400,100,90,50,40,10,9,5,4,1];
-function decimalToRomanSimple(value) {
-  if (value <= 0 || value >= 4000) return value;
-    var romanNumeral = "";
-    for (var i=0; i<roman.length; i++) {
-      while (value >= decimal[i]) { 
-        value -= decimal[i];
-        romanNumeral += roman[i];
+const roman = ["M","CM","D","CD","C","XC","L","XL","X","IX","V","IV","I"];
+const decimal = [1000,900,500,400,100,90,50,40,10,9,5,4,1];
+function convertToRoman(num) {
+  if (num <= 0 || num >= 4000) return num;
+    let romanNumeral = "";
+    for (let index=0; index < roman.length; index += 1) {
+      while (num >= decimal[index]) { 
+        num -= decimal[index];
+        romanNumeral += roman[index];
       }
     }
-    return romanNumeral;
+  return romanNumeral;
 }
 
-console.log(decimalToRomanSimple(3999));
+console.log(convertToRoman(3999));
 
 // console.log(convertToRoman(36));
 
