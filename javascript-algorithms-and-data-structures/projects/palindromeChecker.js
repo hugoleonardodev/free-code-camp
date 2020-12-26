da776555e8fa941f4ab7dbe8251179e812412088
hugoleonardodev <hugoleonardo.dev@gmail.com>
2020-12-26 00:06:14 -0300

    Palindrome Checker

---
+++ javascript-algorithms-and-data-structures/projects/palindromeChecker.js
@@ -0,0 +1,34 @@
+function palindromeChecker(sentence) {
+  const joinedSentence = sentence.replace(/[ ,._-]/g, "");
+  const toLowerCase = joinedSentence.toLowerCase();
+  const sentenceArray = toLowerCase.split('');
+  if (sentenceArray.includes('/')) {
+    const joinSentence = sentenceArray.join('');
+    const removeSpecialChar = joinSentence.replace(/[()\\\/]/g, '');
+    const toArray = removeSpecialChar.split('');
+    const reverse = toArray.reverse();
+    const reverseSentece = reverse.join('');
+    if (reverseSentece === removeSpecialChar) {
+      return true;
+    }
+    return false;
+  }
+  const reverse = sentenceArray.reverse();
+  const reverseSentece = reverse.join('');
+  if (reverseSentece === toLowerCase) {
+    return true;
+  }
+  return false;
+}
+
+console.log(palindromeChecker('_eye'));
+
+console.log(palindromeChecker('race car'));
+
+console.log(palindromeChecker('My age is 0, 0 si ega ym.'));
+
+console.log(palindromeChecker('A man, a plan, a canal. Panama'));
+
+// console.log(palindromeChecker('00(:/:)00');
+
+// console.log(palindromeChecker(`five|\_/|four)`);
---
+++ javascript-algorithms-and-data-structures/projects/tempCodeRunnerFile.js
@@ -0,0 +1,3 @@
+var str = "Text with comma, space, and period.";
+// var res = str.replace(/[ ,.]/g, "");
+// console.log(res);
